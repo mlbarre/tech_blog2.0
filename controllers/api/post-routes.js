0f9a43d3e00592f3553131bc6352f586a1a5fc48
mlb <mlbarrera1538@gmail.com>
2022-02-13 19:04:39 -0600

    added imports of npm packages in server.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -89,16 +89,6 @@ router.post('/', (req, res) => {
     });
 });
 
-// router.put('/upvote', withAuth, (req, res) => {
-//   // custom static method created in models/Post.js
-//   Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-//     .then(updatedVoteData => res.json(updatedVoteData))
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
 router.put('/:id', (req, res) => {
   Post.update(
     {
