46e3e6374f92cd85bc105aa440e7c8d50019fc2d
mlbarre <mlbarrera1538@gmail.com>
2022-02-24 22:22:03 -0600

    added start of express-session

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection.js');
 const SequelizeStore = require('connect-session-sequelize');
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true, 
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
 
 app.use(express.json());
